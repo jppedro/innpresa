fa730a59d9e45a4f5705046a15de98571ea3eab6
jppedro <52078667+jppedro@users.noreply.github.com>
2021-05-27 23:59:51 -0300

    changing category tile

--- innpresa/innpresa/lib/app_state.dart
+++ innpresa/innpresa/lib/app_state.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppState extends ChangeNotifier {
   int selectedCategoryId = 0;
 
-  void updateCategoryId(int selectedCategory) {
+  void updateCategoryId(int selectedCategoryId) {
     this.selectedCategoryId = selectedCategoryId;
     notifyListeners();
   }
